37a68a9866eb9c06cd1ee15117c192104d38575a
Subhanjan Saha <subhanjan.saha@letstransport.team>
2022-01-26 14:42:17 +0530

    Added header key auth functionality

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const { use } = require('express/lib/application');
-const user = require('../models/users')
+const user = require('../models/users');
+const headerkeys = require('../models/header_key_master');
 const router = express.Router();
+
+/*============== Added header key auth middleware function ==============*/
+
+router.use(function(req, res, next){
+    if (req.get('auth_key') === undefined) res.status(401).send('You are not authorized to request this data.');
+    else {
+        headerkeys.findOne({valid_key: req.get('auth_key')}).then(function(headerkey) {
+            if (headerkey === null) res.status(401).send('You are not authorized to request this data.');
+            else next();
+        })
+    }
+})
 
 router.get('/users',function(req, res, next){
     user.find({}).then(function(users) {
@@ -28,5 +40,23 @@ router.delete('/users/:id', function(req, res, next){
     }).catch(next);
 });
 
+/*============== Adding endpoints for Header Key Master ==============*/
 
+router.get('/headerkeys', function(req, res, next){
+    headerkeys.find({}).then(function(headerkey) {
+        res.send(headerkey);
+    }).catch(next);
+})
+
+router.post('/headerkeys', function(req, res, next){
+    headerkeys.create(req.body).then(function(headerkey){
+        res.send(headerkey);
+    }).catch(next);
+})
+
+router.delete('/headerkeys/:key', function(req, res, next){
+    headerkeys.findOneAndDelete({valid_key: req.params.key}).then(function(headerkey) {
+        res.send(headerkey);
+    }).catch(next);
+});
 module.exports = router;
