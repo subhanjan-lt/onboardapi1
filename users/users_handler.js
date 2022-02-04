17e97c1923d7de2596263c40e764a6a8eafb3e46
Subhanjan Saha <subhanjan.saha@letstransport.team>
2022-02-04 22:21:48 +0530

    Changes to UsersHandler

--- users/users_handler.js
+++ users/users_handler.js
@@ -5,23 +5,23 @@ class UsersHandler{
     static init = function (router) {
         UsersMiddleware.use(router);
         
-        router.route('/users').get(function(req, res, next){
-            UsersService.findAll(req, res, next);
+        router.get('/users', async function(req, res, next){
+            await UsersService.findAll(req, res, next);
         });
 
-        router.route('/users/:name').get(function (req, res, next) {
-            UsersService.findOne(req, res, next);
+        router.get('/users/:name', async function (req, res, next) {
+            await UsersService.findOne(req, res, next);
         });
         
-        router.route('/users').put(function(req, res, next){
-            UsersService.create(req, res, next);
+        router.put('/users', async function(req, res, next){
+            await UsersService.create(req, res, next);
         });
         
-        router.route('/users/:id').post(function(req, res, next){
-            UsersService.update(req, res, next);
+        router.post('/users/:id', async function(req, res, next){
+            await UsersService.update(req, res, next);
         });
-        router.route('/users/:id').delete(function(req, res, next){
-            UsersService.delete(req, res, next);
+        router.delete('/users/:id', async function(req, res, next){
+            await UsersService.delete(req, res, next);
         });
     }
 }
